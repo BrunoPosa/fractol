98c2ef84a4a5e51b3364062a0e177b9d423841ce
BrunoPosa <84806733+BrunoPosa@users.noreply.github.com>
2024-05-18 23:54:21 +0300

    Simplified a bit

---
+++ Makefile
@@ -0,0 +1,82 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    Makefile                                           :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: bposa <bposa@student.hive.fi>              +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2024/05/05 18:31:30 by bposa             #+#    #+#              #
+#    Updated: 2024/05/18 23:52:55 by bposa            ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+NAME	= fractol
+
+SRCS	=	graphics.c
+
+CC		= cc
+
+CFLAGS	= -Wall -Wextra -Werror
+
+LIBDIR	= MLX42/
+
+LIBS	= $(LIBDIR)build/libmlx42.a -ldl -lglfw -pthread -lm -L "$$HOME/.brew/opt/glfw/lib/"
+
+HEADERS	= -I $(LIBDIR)include
+
+# OBJCS	= $(SRCS:.c=.o)
+
+all:	$(LIBDIR)build/libmlx42.a $(NAME)
+
+$(NAME): $(SRCS)
+	$(CC) $(CFLAGS) $(SRCS) $(LIBS) $(HEADERS) -o $(NAME)
+
+$(LIBDIR)build/libmlx42.a:
+	cmake $(LIBDIR) -DDEBUG=1 -B $(LIBDIR)build && make -C $(LIBDIR)build -j4
+# Use w/o DEBUG=1 flag for final
+# cmake $(LIBDIR) -B $(LIBDIR)build && make -C $(LIBDIR)build -j4
+
+# %.o: %.c
+# 	$(CC) $(CFLAGS) -Imlx -c $< -o $@
+	
+clean:
+
+fclean: clean
+	rm -f $(NAME)
+	rm -f $(LIBDIR)/build/libmlx42.a
+
+re: fclean all
+
+.PHONY: all re clean fclean
+
+
+
+# ===========================================================
+
+# LIBMLX	:= ./lib/MLX42
+
+# HEADERS	:= -I ./include -I $(LIBMLX)/include
+# LIBS	:= $(LIBMLX)/build/libmlx42.a -ldl -lglfw -pthread -lm
+# OBJS	:= ${SRCS:.c=.o}
+
+# all: libmlx $(NAME)
+
+# libmlx:
+# 	@cmake $(LIBMLX) -B $(LIBMLX)/build && make -C $(LIBMLX)/build -j4
+
+# %.o: %.c
+# 	@$(CC) $(CFLAGS) -o $@ -c $< $(HEADERS) && printf "Compiling: $(notdir $<)"
+
+# $(NAME): $(OBJS)
+# 	@$(CC) $(OBJS) $(LIBS) $(HEADERS) -o $(NAME)
+
+# clean:
+# 	@rm -rf $(OBJS)
+# 	@rm -rf $(LIBMLX)/build
+
+# fclean: clean
+# 	@rm -rf $(NAME)
+
+# re: clean all
+
+# .PHONY: all, clean, fclean, re, libmlx
--- graphics.c
+++ graphics.c
@@ -1,38 +1,17 @@
 #include "./MLX42/include/MLX42/MLX42.h"
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
-#include <math.h>
-#define MAXCOUNT 500
-typedef struct s_pixel_xy_values
-{
-	int	x;
-	int	y;
-}	t_pixel_xy;
+#define WINSIZE 1280
+#define MAXITER 100
+#define SUCCESS 0
+#define ERROR -1
 
-typedef struct s_zoom_values
-{
-	double	x;
-	double	y;
-}	t_zoom;
-
-typedef struct s_redraw_loop_packet
+typedef struct	s_redraw
 {
 	mlx_t		*mlx;
 	mlx_image_t	*img;
-	t_pixel_xy	*pixel;
-	double		*zoomf;
-}	t_redraw_pack;
+	double		zoomf;
+}	t_draw;
 
-
-void	esc_keyhook(mlx_key_data_t keydata, void *param);
-void	scroll_hook(double xdelta, double ydelta, void *param);
-double	pixel_to_plane(char axis, int pixel_value);
-int		if_mandelbrot(double x, double y);
-void	my_pixel_putter(mlx_image_t *img, t_pixel_xy *pixel, double *zoomf);
-
-
-
+void	renderer(t_draw *d);
 
 void	esc_keyhook(mlx_key_data_t keydata, void *param)
 {
@@ -45,118 +24,91 @@ void	scroll_hook(double xdelta, double ydelta, void *param)
 {
 	if (ydelta > 0)
 	{
-		*((t_redraw_pack *)param)->zoomf *= 0.8;
+		((t_draw *)param)->zoomf *= 0.8;
 	}
 	else if (ydelta < 0)
 	{
-		*((t_redraw_pack *)param)->zoomf *= 1.2;
+		((t_draw *)param)->zoomf *= 1.2;
 	}
-	my_pixel_putter(((t_redraw_pack *)param)->img,
-		((t_redraw_pack *)param)->pixel, ((t_redraw_pack *)param)->zoomf);
+	renderer(param);
 	(void)xdelta;
 }
 
-/*
-	pixel_to_plane converts pixel_value x or y into double values
-	scaled to be relevant for a fractal, between -2 and 2.
-	scaled_value = (pix_value * max_pix_value / max_scale_value) - scale_offset
-	[scale_offset is used to move the scaled '0' value towards window center]
-*/
-double	pixel_to_plane(char axis, int pixel_value)
+//make this work with different WINSIZE
+double	scaled(int n)
 {
-	if (axis == 'x' || axis == 'X')
-		return (pixel_value * 0.003125 - 4);
-	return (pixel_value * 0.003125 - 2);
+	return (n * 0.003125 - 2);
 }
-/*
-	X and Y are real and imaginary parts of the complex number, respectively.
-	Imaginary part is Y * sqrt(-1). When the sqroot of -1 is squared, then Y
-	becomes -Y. 
-	explanation++
-*/
-int	if_mandelbrot(double x, double y)
+
+int	my_color(int i)
 {
-	int		count;
+	if (i != 0)
+		return (i * MAXITER / 10 + 0xfff300);
+	else
+		return (0x000000FF);
+}
+
+int	mandelbrot(double x, double y)
+{
+	int		i;
 	double	real;
 	double	imagi;
 	double	temp;
 
-	count = 0;
+	i = 0;
 	real = 0.0;
 	imagi = 0.0;
-	while (real * real + imagi * imagi < 4 && count < MAXCOUNT)
+	while (real * real + imagi * imagi < 4 && i < MAXITER)
 	{
 		temp = real * real - imagi * imagi + x;
 		imagi = 2 * real * imagi + y;
 		real = temp;
-		count++;
+		i++;
 	}
-	if (count < MAXCOUNT)
-		return (count);
-	return (0);
+	if (i < MAXITER)
+		return (i);
+	return (1);
 }
 
-
-// int	colorer(mlx_t *mlx, int i, int j)
-// {
-// 	double	zoom_factor;
-
-// 	zoom_factor = 1.0;
-
-// 	if (zoom_factor != 1.0)
-// 		printf("MORE\n");
-// 	if (if_mandelbrot(pixel_to_plane('x', i) * zoom_factor, pixel_to_plane('y', j) * zoom_factor) == 0)
-// 		return (0x000000FF);
-// 	return (0xFFFFFFFF);
-// }
- 
-void	my_pixel_putter(mlx_image_t *img, t_pixel_xy *pixel, double *zoomf)
+void	renderer(t_draw *d)
 {
-	int color  = 0x000000FF;
-	int	temp = 0;
-	pixel->x = pixel->x / 1920 * *zoomf;
-	pixel->y = pixel->y / 1920 * *zoomf;
+	int		x;
+	int		y;
+	int		i;
 
-	while (pixel->x < 1920)
+	x = 0;
+	y = 0;
+	i = 0;
+	while (x < WINSIZE)
 	{
-		pixel->y = 0;
-		while (pixel->y < 1280)
+		y = 0;
+		while (y < WINSIZE)
 		{
-			temp = if_mandelbrot(pixel_to_plane('x', pixel->x) * *(zoomf), pixel_to_plane('y', pixel->y) * *(zoomf));
-			if (temp != 0)
-				color = temp * MAXCOUNT / 5 + 0xfff300;
-			else
-				color = 0x000000FF;
-			mlx_put_pixel(img, pixel->x, pixel->y, color);
-			pixel->y++;
+			i = mandelbrot(scaled(x) * d->zoomf - 0.75, scaled(y) * d->zoomf);
+			mlx_put_pixel(d->img, x, y, my_color(i)); // d->img->pixels[y * WINSIZE + x] = i * (i / 2);
+			y++;
 		}
-		pixel->x++;
+		x++;
 	}
+	mlx_image_to_window(d->mlx, d->img, 0, 0); // ?
 }
 
+// free(), mlx_terminate() on errors
 int	main(void)
 {
-	mlx_t	*mlx;
-	t_pixel_xy	pixel;
-	double	zoomf = 0.5;
-	t_redraw_pack	redraw;
+	t_draw	draw;
 
-	pixel.x = 0;
-	pixel.y = 0;
-	if (!(mlx = mlx_init(1920, 1280, "fract", false)))
-		return (EXIT_FAILURE);
-	mlx_image_t* img = mlx_new_image(mlx, 1920, 1280);
-	redraw.mlx = mlx;
-	redraw.img = img;
-	redraw.pixel = &pixel;
-	redraw.zoomf = &zoomf;
-	mlx_key_hook(mlx, &esc_keyhook, mlx);
-	mlx_scroll_hook(mlx, &scroll_hook, &redraw);
-	if (!img || (mlx_image_to_window(mlx, img, 0, 0) < 0))
-		return -1;
-	// memset(img->pixels, 255, 1 * sizeof(int32_t));
-	my_pixel_putter(img, &pixel, &zoomf);
-	mlx_loop(mlx);
-	mlx_terminate(mlx);
-	return 0;
+	draw.zoomf = 1.0;
+	draw.mlx = mlx_init(WINSIZE, WINSIZE, "fractol", false);
+	if (!(draw.mlx))
+		return (ERROR);
+	draw.img = mlx_new_image(draw.mlx, WINSIZE, WINSIZE);
+	if (!draw.img || mlx_image_to_window(draw.mlx, draw.img, 0, 0) < 0)
+		return (ERROR);
+	renderer(&draw);
+	mlx_key_hook(draw.mlx, &esc_keyhook, draw.mlx);
+	mlx_scroll_hook(draw.mlx, &scroll_hook, &draw);
+	mlx_loop(draw.mlx);
+	mlx_terminate(draw.mlx);
+	return (SUCCESS); 
 }
