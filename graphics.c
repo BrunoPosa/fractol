8857883b9875b7be3b1781fa01637b91553272b0
BrunoPosa <84806733+BrunoPosa@users.noreply.github.com>
2024-05-20 19:19:19 +0300

    deleted scaled()

--- graphics.c
+++ graphics.c
@@ -8,20 +8,13 @@ typedef struct	s_redraw
 {
 	mlx_t		*mlx;
 	mlx_image_t	*img;
-	long double	zoom;
-	long double	limit;
+	int			limit;
 	double		scale_coef;
-	double		dy[WINSIZE];
-	double		dx[WINSIZE];
+	long double	dy[WINSIZE];
+	long double	dx[WINSIZE];
 }	t_draw;
 
 void	mandel_render(t_draw *d);
-//can i get rid of scaled? store it in the zoomed value of struct? basically, I can call it "mapped" and do all needed for it
-// that way i save some multiplications
-double	scaled(double n, t_draw *d)
-{
-	return (n * d->scale_coef - 2);//- scaled(d->mxpos, d)
-}
 
 void	esc_keyhook(mlx_key_data_t keydata, void *param)
 {
@@ -34,34 +27,42 @@ void	scroll_hook(double xdelta, double ydelta, void *param)
 {
 	t_draw	*d;
 	double	zoomf;
+	int		i;
 
 	d = (t_draw *)param;
 	zoomf = 1.0;
+	i = -1;
 	if (ydelta > 0)
 		zoomf = 0.5;
 	else if (ydelta < 0)
 		zoomf = 1.5;
-	d->zoom *= zoomf;
 	d->limit += 100 * (1.0 - zoomf);
+	while (++i < WINSIZE)
+		d->dx[i] *= zoomf;
+	while (i-- > 0)
+		d->dy[i] *= zoomf;
 	mandel_render(param);
 	(void)xdelta;
 }
 
 int	my_color(int i)
 {
-	return (i << 24 | i << 16 | (i + i) << 8 | 255);
+	return (i << 24 | i << 16 | 220 << 8 | 255);
 }
 
-int	mandelbr(double x, double y, t_draw *d)
+int	mandelbrot(double x, double y, t_draw *d)
 {
-	int		i;
-	double	real;
-	double	imagi;
-	double	temp;
+	int			i;
+	long double	real;
+	long double	imagi;
+	long double	temp;
 
 	i = 0;
 	real = 0.0;
 	imagi = 0.0;
+	temp = 0.0;
+	x = x - 0.74453986035590838011;
+	y = y + 0.12172377389442482241;
 	while (i < d->limit && real < 2.0 && imagi < 2.0)
 	{
 		temp = real * real - imagi * imagi + x;
@@ -76,23 +77,19 @@ int	mandelbr(double x, double y, t_draw *d)
 
 void	mandel_render(t_draw *d)
 {
-	int		x;
-	int		y;
-	int		i;
-	double	xof;
-	double	yof;
+	int	x;
+	int	y;
+	int	i;
 
 	x = 0;
 	y = 0;
 	i = 0;
-	xof = -0.74453986035590838011;
-	yof = 0.12172377389442482241;
 	while (x < WINSIZE)
 	{
 		y = 0;
 		while (y < WINSIZE)
 		{
-			i = mandelbr(d->dx[x] * d->zoom + xof, d->dy[y] * d->zoom + yof, d);
+			i = mandelbrot(d->dx[x], d->dy[y], d);
 			((uint32_t *)d->img->pixels)[y * WINSIZE + x] = my_color(i);
 			y++;
 		}
@@ -108,13 +105,12 @@ int	main(void)
 	int		i;
 
 	i = -1;
-	draw.zoom = 1.0;
 	draw.scale_coef = 4.0 / WINSIZE;
 	draw.limit = 500.0;
 	while (++i < WINSIZE)
-		draw.dx[i] = scaled(i, &draw);
+		draw.dx[i] = i * draw.scale_coef - 2;
 	while (i-- > 0)
-		draw.dy[i] = scaled(i, &draw);
+		draw.dy[i] = i * draw.scale_coef - 2;
 	draw.mlx = mlx_init(WINSIZE, WINSIZE, "fractol", false);
 	if (!(draw.mlx))
 		return (ERROR);
