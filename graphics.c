5acfbdf07462f6e6727f6b10edcb9c0ee06fc271
BrunoPosa <84806733+BrunoPosa@users.noreply.github.com>
2024-05-20 18:19:49 +0300

    Mandel works till PCmax. MAXITER -->Growing limit

--- graphics.c
+++ graphics.c
@@ -1,20 +1,27 @@
 #include "./MLX42/include/MLX42/MLX42.h"
 #define WINSIZE 800
-#define MAXITER 400
 #define SUCCESS 0
 #define ERROR -1
 
+//convert to long double? slower..
 typedef struct	s_redraw
 {
 	mlx_t		*mlx;
 	mlx_image_t	*img;
-	double		zoom;
+	long double	zoom;
+	long double	limit;
 	double		scale_coef;
 	double		dy[WINSIZE];
 	double		dx[WINSIZE];
 }	t_draw;
 
-void	renderer(t_draw *d);
+void	mandel_render(t_draw *d);
+//can i get rid of scaled? store it in the zoomed value of struct? basically, I can call it "mapped" and do all needed for it
+// that way i save some multiplications
+double	scaled(double n, t_draw *d)
+{
+	return (n * d->scale_coef - 2);//- scaled(d->mxpos, d)
+}
 
 void	esc_keyhook(mlx_key_data_t keydata, void *param)
 {
@@ -25,29 +32,27 @@ void	esc_keyhook(mlx_key_data_t keydata, void *param)
 
 void	scroll_hook(double xdelta, double ydelta, void *param)
 {
-	if (ydelta > 0)
-	{
-		((t_draw *)param)->zoom *= 0.9;
-	}
-	else if (ydelta < 0)
-	{
-		((t_draw *)param)->zoom *= 1.1;
-	}
-	renderer(param);
-	(void)xdelta;
-}
+	t_draw	*d;
+	double	zoomf;
 
-double	scaled(int n, t_draw *d)
-{
-	return (n * d->scale_coef - 2);
+	d = (t_draw *)param;
+	zoomf = 1.0;
+	if (ydelta > 0)
+		zoomf = 0.5;
+	else if (ydelta < 0)
+		zoomf = 1.5;
+	d->zoom *= zoomf;
+	d->limit += 100 * (1.0 - zoomf);
+	mandel_render(param);
+	(void)xdelta;
 }
 
 int	my_color(int i)
 {
-	return (i << 24 | i << 16 | (i + i) << 8 | 255);//return (i * i + 1689389995);
+	return (i << 24 | i << 16 | (i + i) << 8 | 255);
 }
 
-int	mandelbrot(double x, double y)
+int	mandelbr(double x, double y, t_draw *d)
 {
 	int		i;
 	double	real;
@@ -57,41 +62,38 @@ int	mandelbrot(double x, double y)
 	i = 0;
 	real = 0.0;
 	imagi = 0.0;
-	while (i < MAXITER && real < 2.0 && imagi < 2.0) // real * real + imagi * imagi < 4
+	while (i < d->limit && real < 2.0 && imagi < 2.0)
 	{
 		temp = real * real - imagi * imagi + x;
 		imagi = 2 * real * imagi + y;
 		real = temp;
 		i++;
 	}
-	if (i < MAXITER)
+	if (i < d->limit)
 		return (i);
 	return (1);
 }
 
-// TODO store y values for WINSIZE in fixed-size array and just iterate over them passing array[i++] to mandelbrot f.
-// to lessen multiplications, kind of like why i have an xtemp variable.
-// zooms into the seahorse valley (offsets x:-0.743643887037151, and y:0.13182590420533)
-void	renderer(t_draw *d)
+void	mandel_render(t_draw *d)
 {
-	int			x;
-	int			y;
-	int			i;
-	double		xof;
-	double		yof;
+	int		x;
+	int		y;
+	int		i;
+	double	xof;
+	double	yof;
 
 	x = 0;
 	y = 0;
 	i = 0;
-	xof = - 0.743643887037249;
-	yof = 0.13182590420533;
+	xof = -0.74453986035590838011;
+	yof = 0.12172377389442482241;
 	while (x < WINSIZE)
 	{
 		y = 0;
 		while (y < WINSIZE)
 		{
-			i = mandelbrot(d->dx[x] * d->zoom + xof, d->dy[y] * d->zoom + yof);
-			((uint32_t *)d->img->pixels)[y * WINSIZE + x] = my_color(i); // TODO could this use i++ to access array elems? instead of y * and + operations?
+			i = mandelbr(d->dx[x] * d->zoom + xof, d->dy[y] * d->zoom + yof, d);
+			((uint32_t *)d->img->pixels)[y * WINSIZE + x] = my_color(i);
 			y++;
 		}
 		x++;
@@ -99,6 +101,7 @@ void	renderer(t_draw *d)
 }
 
 // free(), mlx_terminate() on errors
+// init f()
 int	main(void)
 {
 	t_draw	draw;
@@ -107,6 +110,7 @@ int	main(void)
 	i = -1;
 	draw.zoom = 1.0;
 	draw.scale_coef = 4.0 / WINSIZE;
+	draw.limit = 500.0;
 	while (++i < WINSIZE)
 		draw.dx[i] = scaled(i, &draw);
 	while (i-- > 0)
@@ -117,7 +121,7 @@ int	main(void)
 	draw.img = mlx_new_image(draw.mlx, WINSIZE, WINSIZE);
 	if (!draw.img || mlx_image_to_window(draw.mlx, draw.img, 0, 0) < 0)
 		return (ERROR);
-	renderer(&draw);
+	mandel_render(&draw);
 	mlx_key_hook(draw.mlx, &esc_keyhook, draw.mlx);
 	mlx_scroll_hook(draw.mlx, &scroll_hook, &draw);
 	mlx_loop(draw.mlx);
